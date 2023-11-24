4cadd23183888d98c9d8680d5000db20bf73157e
merahab <ts.baran6161@gmail.com>
2023-11-24 10:13:41 +0300

    First commit

---
+++ js/data.js
@@ -0,0 +1,30 @@
+let urunler = [
+    {
+        "isim": "Pamuk Gömlek",
+        "açıklama": "Rahat ve nefes alabilir pamuk gömlek",
+        "fiyat": 79.99,
+        "fotoğraf": "https://image.api.playstation.com/pr/bam-art/166/422/ae51d535-a526-4ad1-8ca3-9da1dbd19ed8.png?w=620&thumb=false",
+        "stok": 150
+    },
+    {
+        "isim": "Keten Pantolon",
+        "açıklama": "Şık ve rahat keten pantolon",
+        "fiyat": 129.99,
+        "fotoğraf": "https://image.api.playstation.com/vulcan/ap/rnd/202311/2015/22b37959aedcda65054b38b77cd98838b8a498ab72245d1e.png?w=440&thumb=false",
+        "stok": 100
+    },
+    {
+        "isim": "Deri Ceket",
+        "açıklama": "Klasik tarzda deri ceket",
+        "fiyat": 199.99,
+        "fotoğraf": "https://image.api.playstation.com/vulcan/ap/rnd/202208/1718/phwiQjbJddEg979YucUoP3Vr.png?w=440&thumb=false",
+        "stok": 75
+    },
+    {
+        "isim": "Kot Şort",
+        "açıklama": "Yaz ayları için rahat kot şort",
+        "fiyat": 49.99,
+        "fotoğraf": "https://image.api.playstation.com/vulcan/ap/rnd/202304/2714/d7a1f9339c13b4284fcba2acd8d5e4ba2867fa6ef3b8b857.png?w=440&thumb=false",
+        "stok": 120
+    }
+]
